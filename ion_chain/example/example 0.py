4cfbc8672c3bffefcc0a0e6c31dfbe8d6c34868d
Object195 <106102601+Object195@users.noreply.github.com>
2022-06-09 14:39:49 -0500

    Update example 0.py

--- ion_chain/example/example 0.py
+++ ion_chain/example/example 0.py
@@ -3,7 +3,7 @@
 This example intends to show how to import modules and read the documentation of functions in this package
 """
 #%%
-#import the spin subpackage from ion_chain.operator and call it "spin" for futher usage
+#import the spin module from ion_chain.operator subpackage and call it "spin" for further usage
 import ion_chain.operator.spin as spin
 #%%
 #print the discription of the package and list the name of all functions
