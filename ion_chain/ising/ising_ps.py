e372017ed35cf35c1b8af2597aafcd18b429ef24
Object195 <106102601+Object195@users.noreply.github.com>
2022-10-17 23:30:44 -0500

    update ising_ps.py
    Modify in order to be consistent with change in ion_system, verified new code by reproducing the result in summer poster.

--- ion_chain/ising/ising_ps.py
+++ ion_chain/ising/ising_ps.py
@@ -7,33 +7,20 @@ functions: lc, eta, Omega, Axialfreq, Axialmode, Transfreq, Transmode, Jt, plotj
 """
 import numpy as np
 from qutip import *
-from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import ion_chain.operator.spin as spin
-import ion_chain.operator.phonon as phon
-
+from  ion_chain.ising.ion_system import *
+'''
+Define phyiscal constants of the system
+'''
+h = 6.62607015 * 10**(-34) / (2*np.pi)
+MYb171 = 0.171 / (6.02214076*10**23) #mass of Yb ion, kg
+qe = 1.60218 * 10**(-19) #charge of electron, C
+eps0 = 8.85418781 * 10**(-12) #vacuum dielectric constant,SI
+Dk = np.sqrt(2)*2*np.pi / (355*10**(-9)) 
+#difference in wavevector projection of the 2 lasers m-1
+R = (h*Dk**2) / (2*MYb171) #recoil frequency constant, SI 
 def summary():
-    print("____________________________________________________________________")
-    print("function: lc")
-    print("compute the length scale of the system, in SI")
-    print("____________________________________________________________________")
-    print("function: eta")
-    print("Compute single ion Lamb-Dicke parameter for the transvers COM mode.")
-    print("____________________________________________________________________")
-    print("function: Omega")
-    print("compute side band rabi-rate")
-    print("____________________________________________________________________")
-    print("function: Axiafreq")
-    print("compute the eigenfrequencies of Axial oscillation, multiply by wz to get real frequency [Hz]")
-    print("____________________________________________________________________")
-    print("function: Axiamode")
-    print("compute the eigenmodes of Axial oscillation")
-    print("____________________________________________________________________")
-    print("function: Transfreq")
-    print("compute the eigenfrequencies of transverse oscillation, multiply by wz to get real frequency [Hz]")
-    print("____________________________________________________________________")
-    print("function: Transmode")
-    print("compute the eigenmodes of transverse oscillation")
     print("____________________________________________________________________")
     print("function: Jt")
     print("Compute Ising coupling matrix J")
@@ -46,233 +33,14 @@ def summary():
     print("____________________________________________________________________")
     print("function: Hps")
     print("Compute Hamiltonian under a pure spin approximation, with ising coupling constructed only with sx and magentic field coupled with sz")
-#Define phyiscal constants of the system
-h = 6.62607015 * 10**(-34) / (2*np.pi)
-MYb171 = 0.171 / (6.02214076*10**23) #mass of Yb ion, kg
-qe = 1.60218 * 10**(-19) #charge of electron, C
-eps0 = 8.85418781 * 10**(-12) #vacuum dielectric constant,SI
-Dk = np.sqrt(2)*2*np.pi / (355*10**(-9)) 
-#difference in wavevector projection of the 2 lasers m-1
-R = (h*Dk**2) / (2*MYb171) #recoil frequency constant, SI 
-'''
-subfunctions
-'''
 def w(f):
     #transfer to the radial frequency in Hz
     #input fz (MHZ) 
     return 2*np.pi*10**6 * f
-def X0(f0):
-    #compute the characteristic length scale of the motional mode, [m]
-    #input in MHz
-    return np.sqrt(h / (2*MYb171* w(f0)))
-#Compute Transverse and Axial Matrix, eigen modes of the system
-def mask_p(m,N0):
-    #generate a mask array for calculating mth diagonal element
-    mlist = np.ones(N0)
-    mlist[m] = 0
-    return mlist
-#compute equilirium positions
-def E_index(m,N):
-    #generate the indexes for summation for N ions and we are considering the 
-    # mth ion, m,N must be intergers and N>m >=2 
-    e_list = np.zeros(N)
-    e_list[0:m-1] = -1
-    e_list[m:N] = 1
-    return e_list
-def ef_term(xm,xi):
-    #compute the term of electric force with respect to xi, xi is the position
-    #of another ion, xm is the mth index of the position vector
-    return (1/(xm-xi))**2
-def E_eq(x):
-    #generate the list of equations to be solved, x is the position vector of 
-    #the system(list)
-    eqlist = []
-    for m in range(len(x)):
-        elist = E_index(m+1,len(x))
-        new_eq = x[m]
-        for i in range(len(x)):
-            if elist[i] != 0:
-                new_eq =  new_eq + elist[i] * ef_term(x[m],x[i])
-        eqlist.append(new_eq)
-    return eqlist                    
-def E_position(N,fz,scale):
-    '''
-    Compute the equilibrium position of the 1D ion chain centered at 0 
-    Input: 
-        fz, axial frequency of the ion trap
-        N, number of ions in the system
-    Output:
-        np array of N elements representing the equilibrium position of each ion
-    '''
-    x0 = np.zeros(N)
-    if N % 2 == 0:
-        x0[0:int(N/2)] = np.arange(-N/2,0)
-        x0[int(N/2):N] = np.arange(1,(N/2)+1)
-    else:
-        x0 = np.arange((-N-1)/2+1,(N-1)/2+1)
-    esol = fsolve(E_eq, x0)
-    if scale:
-        return np.array(lc(fz)*esol)
-    else:
-        return np.array(esol)
-#print(E_position(1, 3))
-def Aele(N0,m,n,epos):
-    #compute of matrix A at index m 
-    ele_val = 0 
-    if m == n:
-        mlist = mask_p(m,N0)
-        for p in range(N0):
-            if mlist[p] != 0:
-                ele_val = ele_val + 2/(np.abs(epos[m]-epos[p]))**3
-        ele_val = ele_val + 1        
-    else:
-        ele_val = -2/(np.abs(epos[m]-epos[n]))**3    
-    return ele_val
-def Amatrix(N,fz):
-    #compute the tensor A which determines the axial oscillation
-    #fz, axial frequency of the ion trap
-    eposition = E_position(N, fz,False) 
-    Amat = np.zeros((N,N))
-    for m in range(N):
-        for n in range(N):
-            Amat[m,n] = Aele(N,m,n,eposition)
-    return Amat
-def Tmatrix(N,fz,fx):
-    #compute the tensor B that determines transverse oscillation
-    #fx, transverse frequency of the ion trap
-    Amat = Amatrix(N,fz)
-    Tmat = (0.5+(fx/fz)**2) * np.identity(N) - 0.5*Amat
-    return Tmat
+
 '''
 functions to use
 '''
-def lc(fz):
-    '''
-    compute the length scale of the system, in SI
-    input(fz)
-    Parameters
-    ----------
-    fz : flaot
-        axial frequency of the ion trap, [MHz]
-
-    Returns
-    -------
-    float, unit SI
-
-    '''
-    return (qe**2/ (4*np.pi * eps0 * MYb171 * w(fz)**2))**(1/3)
-def eta(fx):
-    '''
-    Compute single ion Lamb-Dicke parameter for the transvers COM mode.
-    input(fx)
-    Parameters
-    ----------
-    fx : float
-        transverse frequency of the ion trap, [MHz]
-
-    Returns
-    -------
-    float, unit SI
-
-    '''
-    return Dk * X0(fx) 
-def Omega(fs,fx):
-    '''
-    Compute side band rabi-rate
-    Input(fs,fx)
-    Parameters
-    ----------
-    fs : float
-        sideband rabi frequency, [kHz]
-    fx : float
-        transverse frequency of the ion trap, [MHz]
-
-    Returns
-    -------
-    float, unit SI
-
-    '''
-    return 2*np.pi*10**3*fs / eta(fx)  
-
-def Axialfreq(N,fz):
-    '''
-    compute the eigenfrequencies of axial oscillation, multiply by wz to get real frequency [Hz]
-    input(N,fz)
-    Parameters
-    ----------
-    N : int
-        number of ions in the system
-    fz : float
-        axial frequency of the ion trap, [MHz]
-
-    Returns
-    -------
-    np array object, each index is an axial eigenfrequency
-
-    '''
-    e_val = np.linalg.eig(Amatrix(N,fz))[0]
-    return np.sqrt(e_val)
-def Axialmode(N,fz):
-    '''
-    compute the eigenmodes of axial oscillation 
-    input(N,fz)
-    Parameters
-    ----------
-    N : int
-        number of ions in the system
-    fz : float
-        axial frequency of the ion trap, [MHz]
-
-    Returns
-    -------
-    np array object that represents N by N matrix, each row is an axial eigenmode
-
-    '''
-    e_array = np.linalg.eig(Amatrix(N,fz))[1]
-    return np.transpose(e_array)
-def Transfreq(N,fz,fx):
-    '''
-    compute the eigenfrequencies of transverse oscillation, multiply by wz to get real frequency [Hz]
-    input(N,fz,fx)
-    Parameters
-    ----------
-    N : int
-        number of ions in the system
-    fz : float
-        axial frequency of the ion trap, [MHz]
-    fx : float
-        transverse frequency of the ion trap, [MHz]
-
-    Returns
-    -------
-    np array object, each index is an transverse eigenfrequency
-
-    '''
-    e_val = np.linalg.eig(Tmatrix(N,fz,fx))[0]
-    #check if the matrix is positive-definite
-    if np.min(e_val) < 0:
-        print("Negtive transverse frequency, the system is unstable")
-    return np.sqrt(e_val)
-def Transmode(N,fz,fx):
-    '''
-    compute the eigenmode of transverse oscillation
-    input(N,fz,fx)
-    Parameters
-    ----------
-    N : int
-        number of ions in the system
-    fz : float
-        axial frequency of the ion trap, [MHz]
-    fx : float
-        transverse frequency of the ion trap, [MHz]
-
-    Returns
-    -------
-    np array object that represents N by N matrix, each row is an transverse eigenmode
-
-    '''
-    e_array = np.linalg.eig(Tmatrix(N,fz,fx))[1]
-    return np.transpose(e_array)
 #Compute Ising coupling matrix J
 def Jt(ion0):
     '''
@@ -293,7 +61,8 @@ def Jt(ion0):
     wx = w(fx); wz = w(fz); sdelta = delta0*2*np.pi*10**3
     Omegar = Omega(fr,fx); Omegab = Omega(fb,fx)
     Omega0 = Omegar*Omegab
-    nfreq = Transfreq(N,fz,fx);emat = Transmode(N,fz,fx)
+    print(Omega0)
+    nfreq = ion0.Transfreq();emat = ion0.Transmode()
     J = np.zeros((N,N))
     for i in range(N):
         for j in range(N):
