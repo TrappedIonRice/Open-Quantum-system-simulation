38141f4e32ca1f4589270d384e4c0a942723532d
Object195 <106102601+Object195@users.noreply.github.com>
2022-12-19 13:05:09 -0600

    Add files via upload
    upload example 3a for excitation transfer

---
+++ ion_chain/example/example 3a.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Compute the time evolution of a 3 ion system contructed to simulate excitation transfer 
+between 2 sites
+@author: zhumj
+"""
+import numpy as np
+from qutip import *
+import matplotlib.pyplot as plt
+import ion_chain.operator.spin as spin
+import ion_chain.operator.phonon as phon
+import ion_chain.transfer.exci_transfer as extrans
+from  ion_chain.ising.ion_system import *
+#%%
+'''
+parameters of the system, in this example, we compute the evoluation at type 1
+reasonance at Delta E = 1*delta_rock
+'''       
+ion_sys = ions() #construct a two ion system using class ions f
+ion_sys.delta_ref = 0
+ion_sys.N = 3
+ion_sys.delta = -100
+ion_sys.fr = 50; ion_sys.fb = 50
+ion_sys.pcut = np.array([8,2,2]) 
+ion_sys.phase = np.pi/2
+ion_sys.gamma = [10,0,0] #cool the rocking mode only
+ion_sys.list_para() #print parameters
+ion_sys.plot_freq()
+#%%
+'''
+define operators for measurement
+'''
+elist = []
+for j in range(3): #phonon population
+    eop = tensor(spin.sI(2),phon.up(j,ion_sys.pcut,ion_sys.N)*phon.down(j,ion_sys.pcut,ion_sys.N))
+    elist.append(eop) 
+oplist = [tensor(spin.sz(2,0),phon.pI(ion_sys.pcut,ion_sys.N)),
+          tensor(spin.sz(2,1),phon.pI(ion_sys.pcut,ion_sys.N)),] #spin population
+elist = oplist+elist    
+#%%    
+'''
+simulation with 1 mode
+'''
+#ion_sys.pcut = [3,2,2]
+#solve time evolution for a single energy splitting
+J23 = 1
+E2 = 100 #set energy difference as 100kHz (\delta_rock)
+E3 = 0
+V = 0
+print('coupling strength between ion 1 and 2', J23, ' kHz *h')
+print('site energy difference ', E2-E3, ' kHz *h')
+configuration = 0 #0 for cooling ion on the side
+tscale = J23      #use J as time scale
+H0, clist1 = extrans.Htot(J23,(E2-E3)/2,0,V,ion_sys,0) #generate Hamiltonian
+rho0 = extrans.rho_ini(ion_sys,True) #initial state
+tplot0 = np.arange(0,2,0.01)
+times0 =tplot0/tscale
+#result = mesolve(H0,rho0,times,clist1,[],progress_bar=True,options=Options(nsteps=10000))
+result1 = mesolve(H0,rho0,times0,clist1,elist,progress_bar=True,options=Options(nsteps=100000))
+#%% plot spin popluation
+pplot1 =  result1.expect[0]
+pplot2 =  result1.expect[1]
+plt.figure(0)
+plt.plot(tplot0,0.5*pplot1+0.5,'-',label=r'$P_{\uparrow\!\!\!\!\downarrow} $')
+plt.plot(tplot0,(0.5*pplot2+0.5),'-',label=r'$P_{\downarrow\!\!\!\!\uparrow} $')
+#plt.xlim(0,800)    
+plt.xlabel(r'$\omega_0t/(2\pi)$')
+plt.ylabel(r'$p_{tot}$')
+title = r'$\delta_{rock} = -100kHz, \Delta E = $'+str(E2) + r'$kHz , J=$'+str(J23)+r'$kHz$'
+plt.title(title)
+plt.yticks(fontsize = 13)
+plt.xticks(fontsize = 13)
+plt.legend(fontsize=10)
+plt.grid()   
+plt.show()
+#%% plot phonon population
+phplot1 =  result1.expect[2]
+phplot2 = result1.expect[3]
+phplot3 =  result1.expect[4]
+#plt.title(title)    
+plt.figure(1)
+plt.plot(times0,phplot1,'r',label='rocking')
+plt.plot(times0,phplot2,'b--',label = 'tilt')
+plt.plot(times0,phplot3,'g--', label = 'COM')
+plt.xlabel(r'$t\delta_0$',fontsize = 14)
+plt.ylabel(r'$<a^+a>$',fontsize = 14)
+plt.title(title)
+plt.yticks(fontsize = 13)
+plt.xticks(fontsize = 13)
+plt.legend(fontsize=10)
+plt.grid()
+plt.show()
